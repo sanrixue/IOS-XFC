bc72ff3dbcd023fead3d06129d01909bfe67a8f8
kantou <744018973@qq.com>
2017-03-15 15:56:49 +0800

    Initial Commit

---
+++ 悬浮仓/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  悬浮仓
+//
+//  Created by 尤超 on 17/3/15.
+//  Copyright © 2017年 尤超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 悬浮仓/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  悬浮仓
+//
+//  Created by 尤超 on 17/3/15.
+//  Copyright © 2017年 尤超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
